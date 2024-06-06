a82a26dca9b588c3eb435d35d2c4fdb8850b0b18
edencoania <edencoania@gmail.com>
2024-06-06 14:54:27 +0300

    all

---
+++ local_ci_react.sh
@@ -0,0 +1,14 @@
+docker build -t my-react-app:latest ./../my-react-app/
+docker tag my-react-app:latest edencoania/release:my-react-apaa
+
+#docker build -t nodejs ./../nodejs/
+#docker tag nodejs:latest edencoania/release:nodejs
+#docker push edencoania/release:nodejs
+kubectl delete deployment.apps/react-app
+
+docker push edencoania/release:my-react-apaa
+docker image rm edencoania/release:my-react-appp
+
+kubectl apply -f reactJS/deployment.yaml
+kubectl apply -f reactJS/service.yaml
+
